f18856545dae72eaf14ee75b91a81f6b1ac1e8ea
Guilheme M Pacheco <guilherme@fpass.com.br>
2021-10-11 01:36:55 -0300

    Add dist

---
+++ dist/models/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./base-event"), exports);
+__exportStar(require("./certificate-type"), exports);
+__exportStar(require("./date-iso"), exports);
+__exportStar(require("./event-type"), exports);
+__exportStar(require("./new-user-request"), exports);
+__exportStar(require("./new-user-request-data"), exports);
+__exportStar(require("./password"), exports);
+__exportStar(require("./resource"), exports);
+__exportStar(require("./resource-type"), exports);
+__exportStar(require("./user-credential-request"), exports);
+__exportStar(require("./user-recover-method"), exports);
+__exportStar(require("./user-recover-request"), exports);
+__exportStar(require("./uuid"), exports);
