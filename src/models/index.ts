80d02ced1041c5fcd010e6449750d2ea94ff2d40
Guilheme M Pacheco <guilherme@fpass.com.br>
2021-10-22 14:26:39 -0300

    v0.1.2

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './create-mfa-dto';
+export * from './create-mfa-event';
+export * from './mfa-extra';
+export * from './verify-mfa-dto';
+export * from './verify-mfa-event';
+export * from './verify-mfa-event-data';
